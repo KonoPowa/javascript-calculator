ba6bfbb6ed829095cc759a77bbecd00f089b2201
Yacob Abusadah <yacob.abusadah@gmail.com>
2020-08-23 16:45:29 -0400

    Updates

---
+++ precache-manifest.55a0464c6809159ffc9a28a18b2570a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e452b01a5e5e596beeccc5fbb131a213",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "85dd8feb8cb59c73715b",
+    "url": "/javascript-calculator/static/css/main.94b53c40.chunk.css"
+  },
+  {
+    "revision": "04f648a882d3b1c030f2",
+    "url": "/javascript-calculator/static/js/2.fa4f79c7.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/javascript-calculator/static/js/2.fa4f79c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85dd8feb8cb59c73715b",
+    "url": "/javascript-calculator/static/js/main.4756762b.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  },
+  {
+    "revision": "a9ee2a5bdadecfa6248a9106158b803f",
+    "url": "/javascript-calculator/static/media/github.a9ee2a5b.png"
+  }
+]);
